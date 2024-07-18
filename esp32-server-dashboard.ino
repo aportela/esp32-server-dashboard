fba3f6ab4c1181c617e15154fbdac02af0528f5c
alex <766f6964+github@gmail.com>
2024-07-18 11:11:21 +0200

    moved display code to external graphic class

--- esp32-server-dashboard.ino
+++ esp32-server-dashboard.ino
@@ -2,96 +2,26 @@
 #include <LovyanGFX.hpp>
 #include <stdint.h>
 
+#include "src/display/ST7789-SPI-240x320-Lovyann/LGFX.hpp"
+#include "src/sources/dummy/DummySource.hpp"
+
 #define TFT_CS 5
 #define TFT_RST 9
 #define TFT_DC 8
 #define TFT_SDA 6
 #define TFT_SCL 4
 
-class LGFX : public lgfx::LGFX_Device
-{
-    lgfx::Panel_ST7789 _panel_instance;
-    lgfx::Bus_SPI _bus_instance;
-
-public:
-    LGFX(void)
-    {
-        auto cfg = _bus_instance.config();
-        cfg.spi_host = SPI2_HOST;
-        cfg.spi_mode = 0;
-        cfg.freq_write = 40000000;
-        cfg.freq_read = 16000000;
-        cfg.spi_3wire = false;
-        cfg.use_lock = true;
-        cfg.dma_channel = 1;
-        cfg.pin_sclk = TFT_SCL;
-        cfg.pin_mosi = TFT_SDA;
-        cfg.pin_miso = -1;
-        cfg.pin_dc = TFT_DC;
-        _bus_instance.config(cfg);
-        _panel_instance.setBus(&_bus_instance);
-
-        auto cfg_panel = _panel_instance.config();
-        cfg_panel.pin_cs = TFT_CS;
-        cfg_panel.pin_rst = TFT_RST;
-        cfg_panel.pin_busy = -1;
-        cfg_panel.memory_width = 240;
-        cfg_panel.memory_height = 320;
-        cfg_panel.panel_width = 240;
-        cfg_panel.panel_height = 320;
-        cfg_panel.offset_x = 0;
-        cfg_panel.offset_y = 0;
-        cfg_panel.offset_rotation = 3;
-        cfg_panel.dummy_read_pixel = 8;
-        cfg_panel.dummy_read_bits = 1;
-        cfg_panel.readable = true;
-        cfg_panel.invert = true;
-        cfg_panel.rgb_order = false;
-        cfg_panel.dlen_16bit = false;
-        cfg_panel.bus_shared = true;
-        _panel_instance.config(cfg_panel);
-
-        setPanel(&_panel_instance);
-    }
-};
 LGFX *screen = nullptr;
 lgfx::LGFX_Sprite *graphSprite = nullptr;
 
 uint8_t values[200];
 
-uint32_t getGradientColor(int value, LGFX* screen)
-{
-    // Asegurarse de que el valor esté dentro del rango 0-100
-    value = constrain(value, 0, 100);
-
-    // Azul muy oscuro a verde (0 a 33)
-    if (value <= 33)
-    {
-        int blue = 255;
-        int green = map(value, 0, 33, 0, 255);
-        return screen->color565(0, green, blue);
-    }
-    // Verde a amarillo (34 a 66)
-    else if (value <= 66)
-    {
-        int green = 255;
-        int red = map(value, 34, 66, 0, 255);
-        return screen->color565(red, green, 0);
-    }
-    // Amarillo a rojo (67 a 100)
-    else
-    {
-        int red = 255;
-        int green = map(value, 67, 100, 255, 0);
-        return screen->color565(red, green, 0);
-    }
-}
-
 void setup()
 {
-    screen = new LGFX();
+    screen = new LGFX(TFT_SDA, TFT_SCL, TFT_CS, TFT_DC, TFT_RST);
     screen->init();
     screen->fillScreen(TFT_BLACK);
+    screen->drawRect(0, 0, 320, 240, TFT_WHITE);
     graphSprite = new lgfx::LGFX_Sprite(screen);
     graphSprite->createSprite(200, 100);
     graphSprite->fillSprite(TFT_BLACK);
@@ -136,13 +66,13 @@ void loop()
     {
         if (values[i] > 0)
         {
-            color = getGradientColor(values[i], screen);
+            color = screen->getGradientColor(values[i]);
             graphSprite->drawFastVLine(i, 98 - values[i], values[i], color);
         }
     }
 
-    screen->setCursor(200, 10);
-    screen->setTextSize(5);
+    screen->setCursor(260, 2);
+    screen->setTextSize(3);
     screen->setTextColor(color, TFT_BLACK);
     screen->setTextWrap(true);
     screen->printf("%02dc", values[199]);
