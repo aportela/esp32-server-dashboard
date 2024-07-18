0ba4a458bdbeb3fb764609984edd73dac0d61d63
alex <766f6964+github@gmail.com>
2024-07-18 11:21:30 +0200

    moved class code to external cpp file

--- src/display/ST7789-SPI-240x320-Lovyann/LGFX.hpp
+++ src/display/ST7789-SPI-240x320-Lovyann/LGFX.hpp
@@ -7,70 +7,7 @@ class LGFX : public lgfx::LGFX_Device
     lgfx::Bus_SPI _bus_instance;
 
 public:
-    LGFX(uint8_t TFT_SDA, uint8_t TFT_SCL, uint8_t TFT_CS, uint8_t TFT_DC, uint8_t TFT_RST)
-    {
-        auto cfg = _bus_instance.config();
-        cfg.spi_host = SPI2_HOST;
-        cfg.spi_mode = 0;
-        cfg.freq_write = 40000000;
-        cfg.freq_read = 16000000;
-        cfg.spi_3wire = false;
-        cfg.use_lock = true;
-        cfg.dma_channel = 1;
-        cfg.pin_sclk = TFT_SCL;
-        cfg.pin_mosi = TFT_SDA;
-        cfg.pin_miso = -1;
-        cfg.pin_dc = TFT_DC;
-        _bus_instance.config(cfg);
-        _panel_instance.setBus(&_bus_instance);
-
-        auto cfg_panel = _panel_instance.config();
-        cfg_panel.pin_cs = TFT_CS;
-        cfg_panel.pin_rst = TFT_RST;
-        cfg_panel.pin_busy = -1;
-        cfg_panel.memory_width = 240;
-        cfg_panel.memory_height = 320;
-        cfg_panel.panel_width = 240;
-        cfg_panel.panel_height = 320;
-        cfg_panel.offset_x = 0;
-        cfg_panel.offset_y = 0;
-        cfg_panel.offset_rotation = 3;
-        cfg_panel.dummy_read_pixel = 8;
-        cfg_panel.dummy_read_bits = 1;
-        cfg_panel.readable = true;
-        cfg_panel.invert = true;
-        cfg_panel.rgb_order = false;
-        cfg_panel.dlen_16bit = false;
-        cfg_panel.bus_shared = true;
-        _panel_instance.config(cfg_panel);
-
-        setPanel(&_panel_instance);
-    }
-
-    uint32_t getGradientColor(int8_t value)
-    {
-        value = constrain(value, 0, 100);
-
-        // blue -> green
-        if (value <= 33)
-        {
-            int blue = 255;
-            int green = map(value, 0, 33, 0, 255);
-            return this->color565(0, green, blue);
-        }
-        // green -> yellow
-        else if (value <= 66)
-        {
-            int green = 255;
-            int red = map(value, 34, 66, 0, 255);
-            return this->color565(red, green, 0);
-        }
-        // yellow -> red
-        else
-        {
-            int red = 255;
-            int green = map(value, 67, 100, 255, 0);
-            return this->color565(red, green, 0);
-        }
-    }
+    LGFX(uint8_t SDA, uint8_t SCL, uint8_t CS, uint8_t DC, uint8_t RST);
+    ~LGFX();
+    uint32_t getGradientColor(int8_t value);
 };
