9aac01073cc3f1303a38abc4d80607abd4066077
alex <766f6964+github@gmail.com>
2024-07-20 10:39:23 +0200

    some tests with new meter

--- esp32-server-dashboard.ino
+++ esp32-server-dashboard.ino
@@ -3,6 +3,7 @@
 #ifdef DISPLAY_DRIVER_LOVYANN_ST7789
 
 #include "src/display/ST7789-SPI-240x320-Lovyann/LGFX.hpp"
+#include "src/display/ST7789-SPI-240x320-Lovyann/LGFXMeter.hpp"
 
 #define PIN_CS 5
 #define PIN_RST 9
@@ -16,6 +17,7 @@
 #define SCREEN_ROTATION 3
 
 LGFX *screen = nullptr;
+LGFXMeter *cpuLoadMeter = nullptr;
 
 #else
 #error NO_DISPLAY_DRIVER
@@ -35,11 +37,12 @@ void setup()
     screen->fillScreen(TFT_BLACK);
     // screen->drawRect(0, 0, 320, 240, TFT_WHITE); // this is for screen bounds debugging purposes only
     screen->initCPULoadMeter(0, 0);
-    screen->initMemoryMeter(0, 42);
-    screen->initCPUTemperatureMeter(0, 84);
-    screen->initNetworkDownloadBandwithMeter(0, 126);
-    screen->initNetworkUploadBandwithMeter(0, 168);
+    // screen->initMemoryMeter(0, 42);
+    // screen->initCPUTemperatureMeter(0, 84);
+    // screen->initNetworkDownloadBandwithMeter(0, 126);
+    // screen->initNetworkUploadBandwithMeter(0, 168);
     screen->setSource(dummySRC);
+    cpuLoadMeter = new LGFXMeter(screen, 200, 30, 0, 42, TFT_BLACK, "CPU LOAD");
 #endif
 }
 
@@ -47,11 +50,12 @@ void loop()
 {
 #ifdef DISPLAY_DRIVER_LOVYANN_ST7789
     dummySRC->refresh();
-    screen->refreshCPULoadMeter(0, 0);
-    screen->refreshMemoryMeter(0, 42);
-    screen->refreshCPUTemperatureMeter(0, 84);
-    screen->refreshNetworkDownloadBandwithMeter(0, 126);
-    screen->refreshNetworkUploadBandwithMeter(0, 168);
+    // screen->refreshCPULoadMeter(0, 0);
+    //   screen->refreshMemoryMeter(0, 42);
+    //   screen->refreshCPUTemperatureMeter(0, 84);
+    //   screen->refreshNetworkDownloadBandwithMeter(0, 126);
+    //   screen->refreshNetworkUploadBandwithMeter(0, 168);
+    cpuLoadMeter->refresh(dummySRC->getCurrentCPULoad());
     screen->refreshDebug(0, 210);
 #else
     delay(50);
