375ae7d7fbf18abfe952c3c76ba38c60dc97f7e9
alex <766f6964+github@gmail.com>
2024-07-23 01:12:54 +0200

    more tests for settings, not working because NVS partition error

--- src/settings/Settings.cpp
+++ src/settings/Settings.cpp
@@ -1,19 +1,122 @@
 #include "Settings.hpp"
+#include <nvs_flash.h>
+#include "Arduino.h"
 
 Settings::Settings(void)
 {
-    this->preferences = new Preferences();
-    this->preferences->begin("esp32-server-dashboard", false);
+    Serial.println("Starting settings");
+    // this->preferences = new Preferences();
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        Serial.println("NVS partition was truncated and needs to be erased. Erasing...");
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    if (err != ESP_OK)
+    {
+        Serial.println("Failed to initialize NVS");
+    }
+    else
+    {
+        Serial.println("NVS OK");
+    }
 }
 
 Settings::~Settings()
 {
-    this->preferences->end();
-    delete this->preferences;
-    this->preferences = nullptr;
+    // delete this->preferences;
+    // this->preferences = nullptr;
+}
+
+void Settings::getValue(const char *key, char *value, size_t count)
+{
+    Serial.println("GET VALUE");
+    Serial.println(key);
+    if (this->preferences.begin("esp32-server-dashboard", false))
+    {
+        if (this->preferences.isKey(key))
+        {
+            this->preferences.getString(key, value, count);
+            Serial.println(value);
+        }
+        else
+        {
+            Serial.println("NOT FOUND");
+            if (count > 0)
+            {
+                value[0] = '\0';
+            }
+        }
+        this->preferences.end();
+    }
+    else
+    {
+        Serial.println("ERROR init preferences");
+    }
+}
+
+void Settings::setValue(const char *key, const char *value)
+{
+    Serial.println("SET VALUE");
+    Serial.println(key);
+    Serial.println(value);
+    if (this->preferences.begin("esp32-server-dashboard", false))
+    {
+        if (this->preferences.putString(key, value))
+        {
+            Serial.println("OK");
+        }
+        else
+        {
+            Serial.println("ERROR");
+        }
+        this->preferences.end();
+    }
+    else
+    {
+        Serial.println("ERROR init preferences");
+    }
+    char a[100];
+    this->getValue(key, a, 100);
 }
 
 void Settings::clear(void)
 {
-    this->preferences->clear();
+    if (this->preferences.begin("esp32-server-dashboard", false))
+    {
+
+        this->preferences.clear();
+        this->preferences.end();
+    }
+    else
+    {
+        Serial.println("ERROR init preferences");
+    }
+}
+
+void Settings::zap(void)
+{
+    nvs_flash_erase(); // erase the NVS partition and...
+    nvs_flash_init();  // initialize the NVS partition.
+}
+
+void Settings::getWIFISSID(char *ssid, size_t count)
+{
+    this->getValue("WIFI_SSID", ssid, count);
+}
+
+void Settings::setWIFISSID(const char *ssid)
+{
+    this->setValue("WIFI_SSID", ssid);
+}
+
+void Settings::getWIFIPassword(char *password, size_t count)
+{
+    this->getValue("WIFI_PASSWORD", password, count);
+}
+
+void Settings::setWIFIPassword(const char *password)
+{
+    this->setValue("WIFI_PASSWORD", password);
 }
--- src/settings/Settings.hpp
+++ src/settings/Settings.hpp
@@ -1,21 +1,28 @@
 #ifndef ESP32_SERVER_DASHBOARD_SETTINGS_H
 #define ESP32_SERVER_DASHBOARD_SETTINGS_H
 
-#include <stdint.h>
+#include <cstdint>
+#include <cstddef>
 #include <Preferences.h>
 
 class Settings
 {
 private:
-    Preferences *preferences;
+    Preferences preferences;
 
-    char WIFI_SSID[32];
-    char WIFI_PASSWORD[64]; // will be enought ?
+    void getValue(const char *key, char *value, size_t count);
+    void setValue(const char *key, const char *value);
 
 public:
     Settings(void);
     ~Settings();
     void clear(void);
+    void zap(void);
+
+    void getWIFISSID(char *ssid, size_t count);
+    void setWIFISSID(const char *ssid);
+    void getWIFIPassword(char *password, size_t count);
+    void setWIFIPassword(const char *password);
 };
 
 #endif
