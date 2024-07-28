5e0f3533c8e9342e9f9f979f33712938282f16f3
alex <766f6964+github@gmail.com>
2024-07-28 16:41:02 +0200

    free resources on destructor

--- src/sources/dummy/DummySource.cpp
+++ src/sources/dummy/DummySource.cpp
@@ -22,6 +22,31 @@ DummySource::DummySource(void) : Source()
 
 DummySource::~DummySource()
 {
+    if (this->currentCPULoad != nullptr)
+    {
+        delete this->currentCPULoad;
+        this->currentCPULoad = nullptr;
+    }
+    if (this->currentMemory != nullptr)
+    {
+        delete this->currentMemory;
+        this->currentMemory = nullptr;
+    }
+    if (this->currentCPUTemperature != nullptr)
+    {
+        delete this->currentCPUTemperature;
+        this->currentCPUTemperature = nullptr;
+    }
+    if (this->currentNetworkDownloadBandwith != nullptr)
+    {
+        delete this->currentNetworkDownloadBandwith;
+        this->currentNetworkDownloadBandwith = nullptr;
+    }
+    if (this->currentNetworkUploadBandwith != nullptr)
+    {
+        delete this->currentNetworkUploadBandwith;
+        this->currentNetworkUploadBandwith = nullptr;
+    }
 }
 
 bool inc = false;
