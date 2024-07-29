59fd6719a9795ecad56b13d5282701233da0bf24
alex <766f6964+github@gmail.com>
2024-07-29 11:39:34 +0200

    use previous saved mqtt telegraf settings

--- esp32-server-dashboard.ino
+++ esp32-server-dashboard.ino
@@ -9,8 +9,8 @@
 
 // these are my custom/valid pin values for a ST7789 display on some esp32 models
 
-// #define ESP32_WROOM
-#define ESP32_C3_SUPER_MINI
+#define ESP32_WROOM
+// #define ESP32_C3_SUPER_MINI
 
 #ifdef ESP32_C3_SUPER_MINI
 // ESP32 C3 SUPER MINI
@@ -38,38 +38,58 @@ LGFX *screen = nullptr;
 #error NO_DISPLAY_DRIVER
 #endif // DISPLAY_DRIVER_LOVYANN_ST7789
 
+#include "src/utils/Settings.hpp"
 #include "src/utils/WifiManager.hpp"
 #include "src/utils/SerialManager.hpp"
 #include "src/sources/dummy/DummySource.hpp"
 #include "src/sources/mqtt/MQTTTelegrafSource.hpp"
-// #include "src/Settings/Settings.hpp"
 #include "src/display/ScreenType.hpp"
+#include <Preferences.h>
 
-Source *dummySRC = nullptr;
+DummySource *dummySRC = nullptr;
 MQTTTelegrafSource *mqttTelegrafSRC = nullptr;
-// Settings *settings = nullptr;
+Settings *settings = nullptr;
 
+Preferences preferences;
 void setup()
 {
-    // settings = new Settings();
+    //  TODO: default info screen if no valid settings found
+    //  TODO: rotary encoder controller, button pressed at boot = enter settings mode, movement = toggle between screens
+
     SerialManager::init(SerialManager::DEFAULT_SPEED);
     Serial.println("Starting esp32-server-dashboard");
-    // TODO: default info screen if no valid settings found
-    // TODO: rotary encoder controller, button pressed at boot = enter settings mode, movement = toggle between screens
-    WifiManager::setCredentials(WIFI_SSID, WIFI_PASSWORD);
+
+    settings = new Settings();
+
+    char WiFiSSID[WIFI_SSID_CHAR_ARR_LENGTH];
+    settings->getWIFISSID(WiFiSSID, WIFI_SSID_CHAR_ARR_LENGTH);
+
+    char WiFiPassword[WIFI_PASSWORD_CHAR_ARR_LENGTH];
+    settings->getWIFIPassword(WiFiPassword, WIFI_PASSWORD_CHAR_ARR_LENGTH);
+
+    WifiManager::setCredentials(WiFiSSID, WiFiPassword);
     WifiManager::connect(true);
-    char mac[32] = {'\0'};
-    WifiManager::getMacAddress(mac, sizeof(mac));
+
     dummySRC = new DummySource();
-    mqttTelegrafSRC = new MQTTTelegrafSource("mqtt://192.168.24.5", mac, "telegraf/HOST_NAME/#");
-    // mqttTelegrafSRC->setCPUTopic("telegraf/HOST_NAME/cpu");
-    // mqttTelegrafSRC->setMemoryTopic("telegraf/HOST_NAME/mem");
+
+    char mqttTelegrafURI[64] = {'\0'};
+    settings->getMQTTTelegrafURI(mqttTelegrafURI, sizeof(mqttTelegrafURI));
+    char mqttTelegrafGlobalTopic[512] = {'\0'};
+    settings->getMQTTTelegrafGlobalTopic(mqttTelegrafGlobalTopic, sizeof(mqttTelegrafGlobalTopic));
+    if (strlen(mqttTelegrafURI) > 0 && strlen(mqttTelegrafGlobalTopic) > 0)
+    {
+        char WiFiMacAddress[32] = {'\0'};
+        WifiManager::getMacAddress(WiFiMacAddress, sizeof(WiFiMacAddress));
+        mqttTelegrafSRC = new MQTTTelegrafSource(mqttTelegrafURI, WiFiMacAddress, mqttTelegrafGlobalTopic);
+    }
+
 #ifdef DISPLAY_DRIVER_LOVYANN_ST7789
 
     screen = new LGFX(PIN_SDA, PIN_SCL, PIN_CS, PIN_DC, PIN_RST, DISPLAY_DRIVER_LOVYANN_ST7789_WIDTH, DISPLAY_DRIVER_LOVYANN_ST7789_HEIGHT, DISPLAY_DRIVER_LOVYANN_ST7789_ROTATION);
     screen->setSource(dummySRC);
 
     screen->initScreen(ST_INFO);
+    // screen->initScreen(ST_DATA_RESUME);
 #endif // DISPLAY_DRIVER_LOVYANN_ST7789
 }
 
@@ -77,6 +97,7 @@ void loop()
 {
     SerialManager::loop();
     WifiManager::loop();
+    dummySRC->refresh();
 #ifdef DISPLAY_DRIVER_LOVYANN_ST7789
     screen->refresh();
 #endif // DISPLAY_DRIVER_LOVYANN_ST7789
