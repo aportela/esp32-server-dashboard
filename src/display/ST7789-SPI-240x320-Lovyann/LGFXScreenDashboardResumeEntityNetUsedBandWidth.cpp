ab392662170b126b03e86ec67de15494baf2016e
alex <766f6964+github@gmail.com>
2024-08-06 23:02:40 +0200

    changes for dynamic bandwidth scaling

--- src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.cpp
+++ src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.cpp
@@ -1,16 +1,50 @@
 #include "LGFXScreenDashboardResumeEntityNetUsedBandWidth.hpp"
 #include "../../utils/Format.hpp"
 
-LGFXScreenDashboardResumeEntityNetUsedBandWidth::LGFXScreenDashboardResumeEntityNetUsedBandWidth(NetBandwidthType type, LovyanGFX *display, SourceData *sourceData, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset) : LGFXScreenDashboardResumeEntity(display, sourceData, width, height, xOffset, yOffset, type == NBT_DOWNLOAD ? "NET DOWNLOAD" : "NET UPLOAD"), type(type)
+LGFXScreenDashboardResumeEntityNetUsedBandWidth::LGFXScreenDashboardResumeEntityNetUsedBandWidth(NetBandwidthType type, LovyanGFX *display, SourceData *sourceData, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, bool dynamicScale) : LGFXScreenDashboardResumeEntity(display, sourceData, width, height, xOffset, yOffset, type == NBT_DOWNLOAD ? "NET DOWNLOAD" : "NET UPLOAD"), type(type), dynamicScale(dynamicScale)
 {
     if (this->parentDisplay != nullptr)
     {
         this->refreshStrValue("0000 Bytes/seg", LGFX_SCR_DRE_FONT_COLOR, LGFX_SCR_DRE_FONT_BG_COLOR);
     }
+    if (dynamicScale)
+    {
+        this->FIFOPreviousValues = (uint64_t *)calloc(width, sizeof(uint64_t));
+        if (this->FIFOPreviousValues == NULL)
+        {
+            Serial.println("Error allocating FIFO for dynamic scaling, switching to default scale");
+            this->dynamicScale = false;
+        }
+        else
+        {
+            this->FIFOFront = 0;
+            this->FIFORear = 0;
+            this->FIFOSize = 0;
+        }
+    }
 }
 
 LGFXScreenDashboardResumeEntityNetUsedBandWidth::~LGFXScreenDashboardResumeEntityNetUsedBandWidth()
 {
+    if (this->FIFOPreviousValues != nullptr)
+    {
+        free(this->FIFOPreviousValues);
+        this->FIFOPreviousValues = nullptr;
+    }
+}
+
+bool LGFXScreenDashboardResumeEntityNetUsedBandWidth::enqueueFIFO(uint64_t value)
+{
+    if (this->FIFOSize == this->width)
+    {
+        this->FIFOFront = (this->FIFOFront + 1) % this->width;
+    }
+    else
+    {
+        this->FIFOSize++;
+    }
+    this->FIFOPreviousValues[this->FIFORear] = value;
+    this->FIFORear = (this->FIFORear + 1) % this->width;
 }
 
 bool LGFXScreenDashboardResumeEntityNetUsedBandWidth::refresh(bool force)
@@ -39,6 +73,28 @@ bool LGFXScreenDashboardResumeEntityNetUsedBandWidth::refresh(bool force)
             currentValue = this->sourceData->getNetworkUploadSpeed();
         }
         this->timestamp = currentTimestamp;
+        bool growScaleRequired = false;
+        if (this->dynamicScale)
+        {
+            while (this->byteScales[this->currentByteScale] < currentValue && this->currentByteScale < sizeof(this->byteScales))
+            {
+                this->currentByteScale++;
+                growScaleRequired = true;
+            }
+            this->enqueueFIFO(currentValue);
+            if (growScaleRequired)
+            {
+                if (this->type == NBT_DOWNLOAD)
+                {
+                    this->sourceData->setNetworkDownloadBandwidthLimit(this->byteScales[this->currentByteScale]);
+                }
+                else
+                {
+                    this->sourceData->setNetworkUploadBandwidthLimit(this->byteScales[this->currentByteScale]);
+                }
+            }
+        }
+
         uint8_t mapped100 = 0;
 
         if (this->type == NBT_DOWNLOAD)
@@ -60,7 +116,22 @@ bool LGFXScreenDashboardResumeEntityNetUsedBandWidth::refresh(bool force)
             this->refreshStrValue(strValue, currentGradientColor, LGFX_SCR_DRE_FONT_BG_COLOR);
             this->value = currentValue;
         }
-        this->refreshSprite(mapped100, currentGradientColor);
+        if (this->dynamicScale)
+        {
+            if (!growScaleRequired)
+            {
+                this->refreshSprite(mapped100, currentGradientColor);
+                // TODO: shrink required ?
+            }
+            else
+            {
+                // TODO: refresh ALL
+            }
+        }
+        else
+        {
+            this->refreshSprite(mapped100, currentGradientColor);
+        }
         return (true);
     }
     else
--- src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.hpp
+++ src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.hpp
@@ -14,33 +14,46 @@ class LGFXScreenDashboardResumeEntityNetUsedBandWidth : public LGFXScreenDashboa
 private:
     uint64_t timestamp = 0;
     uint64_t value = 0;
-    const uint64_t byteScales[10] = {
-        1024,       // 1 Kbyte
-        2048,       // 2 Kbytes
-        4096,       // 4 Kbtes
-        8192,       // 8 Kbytes
-        16384,      // 16 Kbytes
-        32768,      // 32 Kbytes
-        65536,      // 64 Kbytes
-        131072,     // 128 Kbytes
-        262144,     // 256 Kbytes
-        524288,     // 512 Kbytes
-        1048576,    // 1 Mbyte
-        2097152,    // 2 Mbytes
-        5242880,    // 5 Bytes
-        10485760,   // 10 Mbytes
-        20971520,   // 25 Mbytes
-        52428800,   // 50 Mbytes
-        104857600,  // 100 Mbytes
-        209715200,  // 200 Mbytes
-        524288000,  // 500 Mbytes
-        1048576000, // 1 Gbyte
+    bool dynamicScale = false;
+    const uint64_t byteScales[25] = {
+        1024,        // 1 Kbyte
+        2048,        // 2 Kbytes
+        4096,        // 4 Kbtes
+        8192,        // 8 Kbytes
+        16384,       // 16 Kbytes
+        32768,       // 32 Kbytes
+        65536,       // 64 Kbytes
+        131072,      // 128 Kbytes
+        262144,      // 256 Kbytes
+        524288,      // 512 Kbytes
+        1048576,     // 1 Mbyte
+        2097152,     // 2 Mbytes
+        5242880,     // 5 Bytes
+        10485760,    // 10 Mbytes
+        20971520,    // 25 Mbytes
+        52428800,    // 50 Mbytes
+        104857600,   // 100 Mbytes
+        209715200,   // 200 Mbytes
+        524288000,   // 500 Mbytes
+        1048576000,  // 1 Gbyte
+        2097152000,  // 2 Gbytes
+        5242880000,  // 5 Gbytes
+        10485760000, // 10 Gbytes
+        26214400000, // 25 Gbytes
+        52428800000, // 50 Gbytes
     };
     uint8_t currentByteScale = 0;
+    uint64_t *FIFOPreviousValues = nullptr; // temporal FIFO store required for dynamic scaling
+    uint16_t FIFOFront = 0;
+    uint16_t FIFORear = 0;
+    uint16_t FIFOSize = 0;
+
     NetBandwidthType type;
 
+    bool enqueueFIFO(uint64_t value);
+
 public:
-    LGFXScreenDashboardResumeEntityNetUsedBandWidth(NetBandwidthType type, LovyanGFX *display, SourceData *sourceData, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset);
+    LGFXScreenDashboardResumeEntityNetUsedBandWidth(NetBandwidthType type, LovyanGFX *display, SourceData *sourceData, uint16_t width, uint16_t height, uint16_t xOffset, uint16_t yOffset, bool dynamicScale = false);
     ~LGFXScreenDashboardResumeEntityNetUsedBandWidth();
     bool refresh(bool force = false) override;
 };
