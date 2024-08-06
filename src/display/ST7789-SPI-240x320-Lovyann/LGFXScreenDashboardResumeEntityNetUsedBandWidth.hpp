1de308e948976e7c526b4d94425e563acc52ff57
alex <766f6964+github@gmail.com>
2024-08-06 20:05:20 +0200

    new constant for auto scaling bandwidth

--- src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.hpp
+++ src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenDashboardResumeEntityNetUsedBandWidth.hpp
@@ -14,6 +14,29 @@ class LGFXScreenDashboardResumeEntityNetUsedBandWidth : public LGFXScreenDashboa
 private:
     uint64_t timestamp = 0;
     uint64_t value = 0;
+    const uint64_t byteScales[10] = {
+        1024,       // 1 Kbyte
+        2048,       // 2 Kbytes
+        4096,       // 4 Kbtes
+        8192,       // 8 Kbytes
+        16384,      // 16 Kbytes
+        32768,      // 32 Kbytes
+        65536,      // 64 Kbytes
+        131072,     // 128 Kbytes
+        262144,     // 256 Kbytes
+        524288,     // 512 Kbytes
+        1048576,    // 1 Mbyte
+        2097152,    // 2 Mbytes
+        5242880,    // 5 Bytes
+        10485760,   // 10 Mbytes
+        20971520,   // 25 Mbytes
+        52428800,   // 50 Mbytes
+        104857600,  // 100 Mbytes
+        209715200,  // 200 Mbytes
+        524288000,  // 500 Mbytes
+        1048576000, // 1 Gbyte
+    };
+    uint8_t currentByteScale = 0;
     NetBandwidthType type;
 
 public:
