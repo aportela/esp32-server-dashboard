bb23290ed842e37c0df6c266e024f82b3a336441
alex <766f6964+github@gmail.com>
2024-08-17 12:11:13 +0200

    mutex is not working so new event system for testing alternatives

--- src/sources/Source.cpp
+++ src/sources/Source.cpp
@@ -1,5 +1,10 @@
 #include "Source.hpp"
 
+void (*Source::onCPULoadChangedCallback)(float value, uint64_t timestamp) = nullptr;
+void (*Source::onTotalMemoryChangedCallback)(uint64_t value, uint64_t timestamp) = nullptr;
+void (*Source::onUsedMemoryChangedCallback)(uint64_t value, uint64_t timestamp) = nullptr;
+void (*Source::onCPUTemperatureChangedCallback)(float value, uint64_t timestamp) = nullptr;
+
 Source::Source(SourceData *sourceData)
 {
     if (sourceData != nullptr)
@@ -14,4 +19,25 @@ Source::~Source()
     {
         this->sourceData = nullptr;
     }
+    Source::onCPULoadChangedCallback = nullptr;
+}
+
+void Source::onCPULoadChanged(void (*callback)(float value, uint64_t timestamp))
+{
+    Source::onCPULoadChangedCallback = callback;
+}
+
+void Source::onTotalMemoryChanged(void (*callback)(uint64_t value, uint64_t timestamp))
+{
+    Source::onTotalMemoryChangedCallback = callback;
+}
+
+void Source::onUsedMemoryChanged(void (*callback)(uint64_t value, uint64_t timestamp))
+{
+    Source::onUsedMemoryChangedCallback = callback;
+}
+
+void Source::onCPUTemperatureChanged(void (*callback)(float value, uint64_t timestamp))
+{
+    Source::onCPUTemperatureChangedCallback = callback;
 }
--- src/sources/Source.hpp
+++ src/sources/Source.hpp
@@ -12,10 +12,19 @@ class Source : public ISource
 {
 protected:
     SourceData *sourceData = nullptr;
+    static void (*onCPULoadChangedCallback)(float value, uint64_t timestamp);
+    static void (*onTotalMemoryChangedCallback)(uint64_t value, uint64_t timestamp);
+    static void (*onUsedMemoryChangedCallback)(uint64_t value, uint64_t timestamp);
+    static void (*onCPUTemperatureChangedCallback)(float value, uint64_t timestamp);
 
 public:
     Source(SourceData *sourceData);
     virtual ~Source();
+
+    void onCPULoadChanged(void (*callback)(float value, uint64_t timestamp));
+    void onTotalMemoryChanged(void (*callback)(uint64_t value, uint64_t timestamp));
+    void onUsedMemoryChanged(void (*callback)(uint64_t value, uint64_t timestamp));
+    void onCPUTemperatureChanged(void (*callback)(float value, uint64_t timestamp));
 };
 
 #endif
