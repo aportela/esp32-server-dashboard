2c9dc449c501e09ca525538e09202e40bb5c08dc
alex <766f6964+github@gmail.com>
2024-08-19 12:14:42 +0200

    code formattings

--- src/utils/Format.hpp
+++ src/utils/Format.hpp
@@ -21,7 +21,6 @@ private:
 public:
     static void parseFloatIntoCharArray(float value, uint8_t decimalCount, uint8_t leftZeroPaddingCount, char *buffer, size_t buffer_size);
     static void bytesToHumanStr(uint64_t bytes, char *buffer, size_t buffer_size, bool zeroPadding = false, bool shortUnits = false, bool bandwidthUnits = false, KILO_BYTE_DIVISOR kByteDivisorUnit = KBD_1024);
-
     static void millisToHumanStr(uint64_t millis_diff, char *buffer, size_t buffer_size);
 };
 
