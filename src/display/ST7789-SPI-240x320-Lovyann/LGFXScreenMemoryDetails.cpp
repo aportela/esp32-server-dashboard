1433a08f3304af3299f28b6a179f4af833e11b75
alex <766f6964+github@gmail.com>
2024-09-10 11:06:02 +0200

    changed default graph types

--- src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenMemoryDetails.cpp
+++ src/display/ST7789-SPI-240x320-Lovyann/LGFXScreenMemoryDetails.cpp
@@ -20,9 +20,9 @@ LGFXScreenMemoryDetails::LGFXScreenMemoryDetails(LovyanGFX *display, SourceData 
 #endif // DEBUG_FPS
             this->memoryBlock1 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 0, MEMORY_USAGE_TYPE_USED);
             this->memoryBlock2 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 1, MEMORY_USAGE_TYPE_FREE);
-            this->memoryBlock3 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 2, MEMORY_USAGE_TYPE_CACHED);
-            this->memoryBlock4 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 3, MEMORY_USAGE_TYPE_SWAP_FREE);
-            this->memoryBlock5 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 4, MEMORY_USAGE_TYPE_SWAP_CACHED);
+            this->memoryBlock3 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 2, MEMORY_USAGE_TYPE_AVAILABLE);
+            this->memoryBlock4 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 3, MEMORY_USAGE_TYPE_BUFFERED);
+            this->memoryBlock5 = new LGFXScreenDashboardResumeEntityUsedMemory(display, sourceData, METER_GRAPH_WIDTH, METER_GRAPH_HEIGHT, 0, (METER_GRAPH_HEIGHT + 11) * 4, MEMORY_USAGE_TYPE_CACHED);
             this->Refresh(true);
         }
     }
